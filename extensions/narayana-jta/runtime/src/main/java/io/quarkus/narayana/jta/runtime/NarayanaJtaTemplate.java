c636e0eebf81f6342d677fc8bd98afc86c293623
Michael Musgrove <mmusgrov@redhat.com>
2019-07-18 14:52:54 +0100

    JBTM-3146 Make the parsing of XML and Java archives more flexible
    Signed-off-by: Michael Musgrove <mmusgrov@redhat.com>

--- extensions/narayana-jta/runtime/src/main/java/io/quarkus/narayana/jta/runtime/NarayanaJtaTemplate.java
+++ extensions/narayana-jta/runtime/src/main/java/io/quarkus/narayana/jta/runtime/NarayanaJtaTemplate.java
@@ -16,7 +16,6 @@
 
 package io.quarkus.narayana.jta.runtime;
 
-import java.lang.reflect.Field;
 import java.util.Properties;
 
 import org.jboss.logging.Logger;
@@ -46,16 +45,7 @@ public class NarayanaJtaTemplate {
     }
 
     public void setDefaultProperties(Properties properties) {
-        //TODO: this is a huge hack to avoid loading XML parsers
-        //this needs a proper SPI
-        try {
-            Field field = PropertiesFactory.class.getDeclaredField("delegatePropertiesFactory");
-            field.setAccessible(true);
-            field.set(null, new QuarkusPropertiesFactory(properties));
-
-        } catch (Exception e) {
-            log.error("Could not override transaction properties factory", e);
-        }
+        PropertiesFactory.setDelegatePropertiesFactory(new QuarkusPropertiesFactory(properties));
 
         defaultProperties = properties;
     }
