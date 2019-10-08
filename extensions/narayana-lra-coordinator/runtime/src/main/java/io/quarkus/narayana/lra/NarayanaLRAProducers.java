51e04652cc22c00917d56c254b5ff41da2c5a23d
Michael Musgrove <mmusgrov@redhat.com>
2019-10-08 16:11:30 +0100

    issue4400 working lra coordinator and participant

---
+++ extensions/narayana-lra-coordinator/runtime/src/main/java/io/quarkus/narayana/lra/NarayanaLRAProducers.java
@@ -0,0 +1,13 @@
+package io.quarkus.narayana.lra;
+
+import javax.enterprise.context.Dependent;
+
+@Dependent
+public class NarayanaLRAProducers {
+    /*
+     * @Produces
+     * public LRAParticipantRegistry lraParticipantRegistry() {
+     * return null;
+     * }
+     */
+}
