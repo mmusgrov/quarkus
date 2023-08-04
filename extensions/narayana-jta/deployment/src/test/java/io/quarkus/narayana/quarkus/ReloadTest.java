d2c4a6b6ae5ee97d6c97af6dba1b7cd0ff9b6e45
Michael Musgrove <mmusgrov@redhat.com>
2023-08-04 11:54:58 +0100

    can't get hot reload to work with transactions

--- extensions/narayana-jta/deployment/src/test/java/io/quarkus/narayana/quarkus/ReloadTest.java
+++ extensions/narayana-jta/deployment/src/test/java/io/quarkus/narayana/quarkus/ReloadTest.java
@@ -1,22 +1,51 @@
 package io.quarkus.narayana.quarkus;
 
-import org.jboss.shrinkwrap.api.asset.StringAsset;
+import jakarta.enterprise.context.control.ActivateRequestContext;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import io.quarkus.test.QuarkusDevModeTest;
+import io.quarkus.test.QuarkusUnitTest;
 
 public class ReloadTest {
-    @RegisterExtension
-    public static final QuarkusDevModeTest test = new QuarkusDevModeTest()
-            .withApplicationRoot((jar) -> {
-                jar
-                        .add(new StringAsset("greeting.message=hello\n"),
-                                "application.properties");
-            });
+    //    @RegisterExtension
+    static final QuarkusUnitTest configWorks = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class));
+    //    @RegisterExtension
+    static final QuarkusDevModeTest config1 = new QuarkusDevModeTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class))
+            .withApplicationRoot((jar) -> jar
+                    .addAsResource("reload.properties", "application.properties"));
+    //    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class))
+            .withApplicationRoot((jar) -> jar
+                    .addAsResource("reload.properties", "application.properties"));
+
+    //    @RegisterExtension // produces a NPE at QuarkusTransactionImpl.java:222
+    final static QuarkusDevModeTest config3 = new QuarkusDevModeTest()
+            .withApplicationRoot((jar) -> jar
+                    .addAsResource("reload.properties", "application.properties"));
+
+    @RegisterExtension // produces a NPE at QuarkusTransactionImpl.java:222
+    static final QuarkusDevModeTest config4 = new QuarkusDevModeTest()
+            .withApplicationRoot(root -> root
+                    .addAsResource("reload.properties", "application.properties"));
 
     @Test
+    @ActivateRequestContext
     public void testHotReplacement() {
-        test.modifyResourceFile("application.properties", s -> s.replace("hello", "goodbye"));
+        //        QuarkusTransaction.begin();
+        //        QuarkusTransaction.commit();
+        config4.modifyResourceFile("application.properties", s -> s.replace("hello", "goodbye"));
+        /*
+         * config.modifyResourceFile("application.properties", s -> s.replace("hello", "goodbye"));
+         * config1.modifyResourceFile("application.properties", s -> s.replace("hello", "goodbye"));
+         * config2.modifyResourceFile("application.properties", s -> s.replace("hello", "goodbye"));
+         * System.out.printf("done%n");
+         */
     }
 }
